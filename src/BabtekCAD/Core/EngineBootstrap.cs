0c5ced96b81c37e2b0c6f7418ae1c0aa0e37aa5c
babtekgroup-hue <babtekgroup@gmail.com>
2026-05-13 21:17:50 +0100

    Create EngineBootstrap.cs

---
+++ src/BabtekCAD/Core/EngineBootstrap.cs
@@ -0,0 +1,10 @@
+namespace BabtekCAD.Core
+{
+    public class EngineBootstrap
+    {
+        public void Run()
+        {
+            System.Console.WriteLine("Engine Started");
+        }
+    }
+}
